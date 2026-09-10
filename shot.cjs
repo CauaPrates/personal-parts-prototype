a416db665da6d2277845d2e15b264ff3d3c35efd
caua.prates <caua.prates@zello.tec.br>
2026-09-10 00:20:56 -0300

    shot

--- shot.cjs
+++ shot.cjs
@@ -5,15 +5,18 @@ const puppeteer = require('puppeteer-core');
     executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
     headless: 'new',
     args: ['--no-sandbox', '--disable-gpu'],
-    defaultViewport: { width: 1440, height: 900, deviceScaleFactor: 1.5 },
+    defaultViewport: { width: 1440, height: 900, deviceScaleFactor: 4 },
   });
   const page = await browser.newPage();
   await page.goto('http://localhost:5173/', { waitUntil: 'networkidle0' });
   await new Promise((r) => setTimeout(r, 300));
-
-  await page.evaluate(() => window.scrollTo({ top: 1148, behavior: 'instant' }));
+  await page.evaluate(() => window.scrollTo({ top: 1062, behavior: 'instant' }));
   await new Promise((r) => setTimeout(r, 500));
 
-  await page.screenshot({ path: process.argv[2] || 'shot.png' });
+  const badges = await page.$$('a[href*="produtos?categoria"] span.flex, a[href*="assunto=pintura"] span.flex');
+  console.log('found', badges.length);
+  for (let i = 0; i < badges.length; i++) {
+    await badges[i].screenshot({ path: `${process.argv[2] || 'icon'}-${i}.png` });
+  }
   await browser.close();
 })();
